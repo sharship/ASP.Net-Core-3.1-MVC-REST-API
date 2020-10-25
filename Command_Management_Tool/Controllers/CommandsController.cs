c7a1c258408f6269c5c418106c6d6952c165db72
sharkship <mryuchangrui@gmail.com>
2020-10-25 12:01:31 -0400

    initial setup

--- Command_Management_Tool/Controllers/CommandsController.cs
+++ Command_Management_Tool/Controllers/CommandsController.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
+using Command_Management_Tool.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Command_Management_Tool.Controllers
 {
-    public class CommandsController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
     {
-        
+
+        // GET: api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            return new List<Command> {
+                new Command {Id = 1, HowTo="yes", Line = "yep", Platform = "YY"},
+                new Command {Id = 2, HowTo="no", Line = "nope", Platform = "NN"}
+            };
+        } 
+              
     }
+
+
 }
--- Command_Management_Tool/Models/Command.cs
+++ Command_Management_Tool/Models/Command.cs
@@ -1,7 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Command_Management_Tool.Models
 {
     public class Command
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string HowTo { get; set; }
+
+        [Required]
+        public string Line { get; set; }
+        
+        [Required]
+        public string Platform { get; set; }
         
     }
 }
